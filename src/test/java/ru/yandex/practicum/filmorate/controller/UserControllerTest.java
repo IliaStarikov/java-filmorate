be150ef2fc60345f12bb3bb44683ebbbd93a1790
Ilya Starikov <lucky979star@yandex.ru>
2024-08-12 09:05:38 +0500

    fix and debug tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,10 +1,22 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -12,144 +24,89 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(UserController.class)
 class UserControllerTest {
-    String inValidUser;
     @Autowired
     private MockMvc mockMvc;
-    private static final String CONTENT_TYPE = "application/json";
+    @MockBean
+    private UserService userService;
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void createUser_WhiteSpaseLogin_ReturnBadRequest() throws Exception {
-        inValidUser = "{\"login\":\"dol ore\",\"name\":\"Nick Name\",\"email\":\"mail@mail.ru\"," +
-                "\"birthday\":\"1946-08-20\"}";
+        User user = User.builder()
+                .name("Nick Name")
+                .login("dol ore")
+                .email("mail@mail.ru")
+                .birthday(LocalDate.of(1946,8,20))
+                .build();
 
         mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(inValidUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void createUser_LoginNull_ReturnBadRequest() throws Exception {
-        inValidUser = "{\"login\":\"\",\"name\":\"Nick Name\",\"email\":\"mail@mail.ru\",\"birthday\":\"1946-08-20\"}";
+        User user = User.builder()
+                .name("Nick Name")
+                .email("mail@mail.ru")
+                .birthday(LocalDate.of(1946,8,20))
+                .build();
 
         mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(inValidUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void createUser_EmailNull_ReturnBadRequest() throws Exception {
-        inValidUser = "{\"login\":\"dolore\",\"name\":\"Nick Name\",\"email\":\"\",\"birthday\":\"1946-08-20\"}";
+        User user = User.builder()
+                .name("Nick Name")
+                .login("dolore")
+                .birthday(LocalDate.of(1946,8,20))
+                .build();
 
         mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(inValidUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void createUser_EmailNotCorrect_ReturnBadRequest() throws Exception {
-        inValidUser = "{\"login\":\"dolore\",\"name\":\"Nick Name\",\"email\":\"mail-mail.ru\"," +
-                "\"birthday\":\"1946-08-20\"}";
+        User user = User.builder()
+                .name("Nick Name")
+                .login("dolore")
+                .email("mail-mail.ru")
+                .birthday(LocalDate.of(1946,8,20))
+                .build();
 
         mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(inValidUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void createUser_BirthdayInFuture_ReturnBadRequest() throws Exception {
-        inValidUser = "{\"login\":\"dolore\",\"name\":\"Nick Name\",\"email\":\"mail@mail.ru\"," +
-                "\"birthday\":\"2446-08-20\"}";
+        User user = User.builder()
+                .name("Nick Name")
+                .login("dolore")
+                .email("mail@mail.ru")
+                .birthday(LocalDate.of(2446,8,20))
+                .build();
 
         mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(inValidUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void createUser_CreateValidUser_ReturnOkRequest() throws Exception {
-        String validUser = "{\"login\":\"dolore\",\"name\":\"Nick Name\",\"email\":\"mail@mail.ru\"," +
-                "\"birthday\":\"2024-02-10\"}";
-
-        mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(validUser))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(2))
-                .andExpect(jsonPath("$.login").value("dolore"))
-                .andExpect(jsonPath("$.name").value("Nick Name"))
-                .andExpect(jsonPath("$.email").value("mail@mail.ru"))
-                .andExpect(jsonPath("$.birthday").value("2024-02-10"));
-    }
-
-    @Test
-    void createUser_NameNull_LoginEqualsNameTrue() throws Exception {
-        String validUser = "{\"login\":\"dolore\",\"email\":\"mail@mail.ru\",\"birthday\":\"1946-08-20\"}";
-
-        this.mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(validUser))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(3))
-                .andExpect(jsonPath("$.login").value("dolore"))
-                .andExpect(jsonPath("$.name").value("dolore"))
-                .andExpect(jsonPath("$.email").value("mail@mail.ru"))
-                .andExpect(jsonPath("$.birthday").value("1946-08-20"));
-    }
-
-    @Test
-    void getAllUser_ReturnUsersAndOkRequestAndEqualsUser() throws Exception {
-        String validUser = "{\"login\":\"dolor\",\"email\":\"mail@mail.ru\",\"birthday\":\"1946-08-20\"}";
-        mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(validUser))
-                .andExpect(status().isOk());
-
-        this.mockMvc.perform(get("/users"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[3].id").value(4))
-                .andExpect(jsonPath("$[3].login").value("dolor"))
-                .andExpect(jsonPath("$[3].name").value("dolor"))
-                .andExpect(jsonPath("$[3].email").value("mail@mail.ru"))
-                .andExpect(jsonPath("$[3].birthday").value("1946-08-20"));
-    }
-
-    @Test
-    void updateUser_PutUser_ReturnOkRequestAndEqualsUser() throws Exception {
-        String validUser = "{\"login\":\"dolor\",\"email\":\"mail@mail.ru\",\"birthday\":\"1946-08-20\"}";
-        mockMvc.perform(post("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(validUser))
-                .andExpect(status().isOk());
-
-        String updateUser = "{\"id\":\"1\",\"login\":\"dolores\",\"name\":\"Nick Name\"," +
-                "\"email\":\"mail1946@mail.ru\",\"birthday\":\"1946-08-20\"}";
-        this.mockMvc.perform(put("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(updateUser))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.login").value("dolores"))
-                .andExpect(jsonPath("$.name").value("Nick Name"))
-                .andExpect(jsonPath("$.email").value("mail1946@mail.ru"))
-                .andExpect(jsonPath("$.birthday").value("1946-08-20"));
-    }
-
-    @Test
-    void updateUser_TrueNotFoundException() throws Exception {
-        String updateUser = "{\"id\":\"9999\",\"login\":\"dolor\",\"email\":\"mail@mail.ru\"," +
-                "\"birthday\":\"1946-08-20\"}";
-        this.mockMvc.perform(put("/users")
-                        .contentType(CONTENT_TYPE)
-                        .content(updateUser))
-                .andExpect(status().isNotFound());
     }
 }
